1dec9703f63c81e734d319e3ce950e61c6515e9b
assiszang <assiszang@gmail.com>
2026-02-26 08:27:01 -0300

    feat: add new project meadowlark

---
+++ meadowlark/site/meadowlark.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.type('text/plain')
+    res.send('Meadowlark Travel');
+});
+
+app.get('/about', (req, res) => {
+    res.type('text/plain')
+    res.send('About Meadowlark Travel')
+});
+
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
+});
+
+const port = process.env.PORT || 3000;
+
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+});
+
+app.listen(port, () => console.log(`Express started on http://localhost:${port}; `
+    + `press Ctrl-C to terminate.`));
